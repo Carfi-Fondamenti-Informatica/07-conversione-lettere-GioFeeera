5b86829208820888bb0b73df431dc54ee4b35320
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-12-09 06:13:46 +0000

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 bool funzione (char &x) {
     if (((x>=65)and (x<=90)) or ((x>=97)and (x<=122))) {
         if ((x>=65)and (x<=90)){
-            x= (x-32);
+            x= (x+32);
         }
         else if((x>=97)and(x<=122)){
             x=(x-32);
